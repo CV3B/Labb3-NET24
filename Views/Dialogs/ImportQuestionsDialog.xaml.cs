2d53ec98454a43615cd2d4e161087a3dc2cc9cb1
cv3b <calle@bjureblad.com>
2024-11-11 21:54:12 +0100

    a

---
+++ Views/Dialogs/ImportQuestionsDialog.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Labb3_NET24.Views.Dialogs;
+
+public partial class ImportQuestionsDialog : Window
+{
+    public ImportQuestionsDialog()
+    {
+        InitializeComponent();
+    }
+}
